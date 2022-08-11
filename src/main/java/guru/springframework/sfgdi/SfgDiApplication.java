ff59697a5b2a679b434cedc08cad71721bc2e6c2
Rishi Singh <rishisingh@monocept.com>
2022-08-12 00:33:38 +0530

    Dependency Injection Tutorials

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controller.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx =  SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		System.out.println("-------Primary Bean");
+
+		MyController mycontroller = (MyController) ctx.getBean("myController");
+		System.out.println(mycontroller.sayHello());
+
+		System.out.println("-----Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("-----Setter");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------Constructor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
+	}
+
+}
---
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controller;
+
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    private final GreetingService greetingService;
+
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
